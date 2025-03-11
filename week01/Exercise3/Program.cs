1c366c2093ad677a97f7289d891ffa1017519630
ZyperHex <kevinherrerach.02@gmail.com>
2025-03-11 00:08:21 -0300

    attempts code added in exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -8,16 +8,19 @@ class Program
         int magicNumber = randomGenerator.Next(1, 101);
 
         int guess = -1;
+        int attempts = 0;
 
 
         while (guess != magicNumber)
         {
             Console.Write("What is your guess? ");
             guess = int.Parse(Console.ReadLine());
+            attempts ++;
 
             if (magicNumber > guess)
             {
                 Console.WriteLine("Higher");
+
             }
             else if (magicNumber < guess)
             {
@@ -25,7 +28,7 @@ class Program
             }
             else
             {
-                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"You guessed it in {attempts} attempts!");
             }
 
         }                    
