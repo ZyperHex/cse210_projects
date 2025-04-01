9a2228b3c65bbfd67c1169c722a1670bdbeead91
ZyperHex <kevinherrerach.02@gmail.com>
2025-03-31 23:51:02 -0300

    Inheritance-Homework activity finished

---
+++ week05/Homework/MathAssignment.cs
@@ -0,0 +1,28 @@
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems) : base(studentName, topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    public string GetTextBookSection()
+    {
+        return _textbookSection;        
+    }
+    public string GetProblems()
+    {
+        return _problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"Section: {GetTextBookSection()} - Problems: {GetProblems()}";
+
+    }
+
+
+}
